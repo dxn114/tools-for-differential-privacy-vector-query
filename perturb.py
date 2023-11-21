17b8f681fafa362e73a366b240327c0745ff6b34
LVXVITAE <1030145861@qq.com>
2023-11-21 17:36:56 +0800

    Changes to be committed: 	modified:   perturb.py
    Changes not staged for commit:
    	modified:   DPHREG.py
    	modified:   tests/test_DPHREG.py
    	modified:   tests/test_HREG.py

--- perturb.py
+++ perturb.py
@@ -2,34 +2,43 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import time
+from joblib import Parallel, delayed
 from tqdm import trange
 t = time.time()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-exp = 3
+exp = 4
 data = torch.randint(0,256,size=(10**exp,128),device=device,dtype=float)
 pw_dist = torch.cdist(data,data).to('cpu').numpy()
-
+def perturb_i(i,idx,pw_dist_new):
+    # f, g for all v in the ith perturbation
+    fgi = np.zeros((data.shape[0],2),dtype=int)
+    for v in range(data.shape[0]):
+        if v == idx:
+            continue
+        old_dist_v = pw_dist[idx,v]
+        new_dist_v = pw_dist_new[i,v]
+        f = (pw_dist[v]<old_dist_v).sum()
+        g = (pw_dist[v]<new_dist_v).sum()
+        if old_dist_v<new_dist_v: 
+            g-=1
+        if f > g:
+            f,g = g,f
+        fgi[v] = f,g
+    return fgi
 def perturb(indices,new,K):
     perturb_time = new.shape[0]
     freqK = np.zeros((len(K),perturb_time),dtype=int)
     pw_dist_new = torch.cdist(new,data).to('cpu').numpy()
-    for i in trange(perturb_time):
-        idx = indices[i]
+    # fg[i] is the f,g for all v in the ith perturbation
+    fg = Parallel(n_jobs=-1)(delayed(perturb_i)(i,indices[i],pw_dist_new) for i in trange(perturb_time))
+    for i in range(perturb_time):
         for v in range(data.shape[0]):
-            if v == idx:
+            if v == indices[i]:
                 continue
-            old_dist_v = pw_dist[idx,v]
-            new_dist_v = pw_dist_new[i,v]
-            f = (pw_dist[v]<old_dist_v).sum()
-            g = (pw_dist[v]<new_dist_v).sum()
-            if old_dist_v<new_dist_v: 
-                g-=1
-            if f > g:
-                f,g = g,f
+            f,g = fg[i][v]
             for j,k in enumerate(K):
                 if f<=k and g>k:
-                    freqK[j,i]+=1
-    
+                    freqK[j,i]+=1    
     return freqK
 
 def one_perturb(k=None):
@@ -43,8 +52,8 @@ def one_perturb(k=None):
     plt.savefig(f"randvec/10^{exp}/perturb1.png")
     plt.close()
 
-def random_perturb(perturb_time=1000):
-    K = [50,100,150,200]
+def random_perturb(perturb_time=10000):
+    K = [10,20,50,100]
     new = torch.randint(0,256,size=(perturb_time,128),device=device,dtype=float)
     indices = np.random.randint(0,10**exp,size=(perturb_time,))
     freqK = perturb(indices,new,K)
@@ -60,6 +69,6 @@ def random_perturb(perturb_time=1000):
     plt.legend()
     plt.savefig(f"randvec/10^{exp}/random_perturb_freqk.png")
 
-random_perturb(perturb_time=10000)
+random_perturb(perturb_time=3000)
 t = time.time()-t
 print(f"Time taken: {t} seconds")
